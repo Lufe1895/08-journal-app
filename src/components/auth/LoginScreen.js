393cac4d7a9a76d1b7a11270312b6a69be26eeb7
Luis Fernando Antonio Toral <luislecompt@gmail.com>
2021-11-13 09:18:02 -0600

    Ayer lo olvidé

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,17 +1,36 @@
+import { useDispatch } from "react-redux"
 import { NavLink } from "react-router-dom"
+import { login } from "../../actions/auth"
+import { useForm } from "../../hooks/useForm"
 
 export const LoginScreen = () => {
+    const [formValues, handleInputChange] = useForm({
+        email: 'luislecompt@gmail.com',
+        password: '123456789'
+    })
+
+    const dispatch = useDispatch();
+
+    const { email, password } = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        dispatch(login(12345, 'Luis'));
+    }
+
     return (
         <>
             <h3 className="auth__title">Login</h3>
 
-            <form>
+            <form onSubmit={ handleLogin }>
                 <input
                     type="text"
                     placeholder="E-mail"
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={ email }
+                    onChange={ handleInputChange }
                 />
 
                 <input
@@ -19,6 +38,8 @@ export const LoginScreen = () => {
                     placeholder="Password"
                     name="email"
                     className="auth__input"
+                    value={ password }
+                    onChange={ handleInputChange }
                 />
 
                 <button
