b4676d84c28179f201e5d9743f77ea73ae9c575f
Luis Fernando Antonio Toral <luislecompt@gmail.com>
2021-11-16 12:25:04 -0600

    Hora de comida

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,5 +1,7 @@
-import { addDoc, collection } from "@firebase/firestore";
+import { addDoc, collection, doc, setDoc } from "@firebase/firestore";
+import Swal from "sweetalert2";
 import { db } from "../firebase/firebase-config";
+import { fileUpload } from "../helpers/fileUpload";
 import { loadNotes } from "../helpers/loadNotes";
 import { types } from "../types/types";
 
@@ -37,3 +39,38 @@ export const setNotes = (notes) => ({
     type: types.notesLoad,
     payload: notes,
 });
+
+export const startSaveNote = (note) => {
+    return async(dispatch, getState) => {
+        const { uid } = getState().auth;
+
+        if(!note.url) {
+            delete note.url;
+        }
+
+        const noteToFirestore = { ...note };
+        delete noteToFirestore.id;
+        await setDoc(doc(db, `${ uid }/journal/notes/${ note.id }`), noteToFirestore);
+        dispatch(refreshNote(note.id, noteToFirestore));
+        Swal.fire('Saved', note.title, 'success');
+    }
+}
+
+export const refreshNote = (id, note) => ({
+    type: types.noteUpdate,
+    payload: {
+        id,
+        note: {
+            id,
+            ...note
+        }
+    }
+})
+
+export const startUploading = (file) => {
+    return async(dispatch, getState) => {
+        const { active: activeNote } = getState().notes;
+        const fileUrl = await fileUpload(file);
+        console.log(fileUrl);
+    }
+}
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,11 +1,27 @@
-import { useSelector } from "react-redux"
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { activeNote } from "../../actions/notes";
 import { useForm } from "../../hooks/useForm";
 import { NotesAppBar } from "./NotesAppBar"
 
 export const NoteScreen = () => {
     const { active: note } = useSelector(state => state.notes);
-    const [formValues, handleInputChange] = useForm(note);
+    const [formValues, handleInputChange, reset] = useForm(note);
     const { title, body } = formValues;
+    const activeId = useRef(note.id);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(note.id !== activeId.current) {
+            reset(note);
+            activeId.current = note.id;
+        }
+    }, [reset, note]);
+
+    useEffect(() => {
+        dispatch(activeNote(formValues.id, { ...formValues }));
+    }, [formValues, dispatch])
+
     return (
         <div className="notes__main-content">
             <NotesAppBar />
